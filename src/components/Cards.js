aefc67ad265b9d50cbac62513e40f3421e5673aa
darrenthamtoro <darrenthamtoro@gmail.com>
2021-08-29 21:41:29 -0700

    main page initial wireframe

---
+++ src/components/Cards.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+export default function Cards() {
+    const linkStyle = { border: '1px black', padding: '5px' };
+
+    return (
+        <button class="card">
+            <section id="img1">
+                <img id="chiefs-logo" src="" alt="" width="200" height="100">>
+                <div class="container">
+                    <h1>KANSAS CITY CHIEFS</h1>
+                </div>
+            </section>
+        </button>
+        <button class="card">
+            <section id="img1">
+                <img id="seahawks-logo" src="" alt="" width="200" height="100">>
+                <div class="container">
+                    <h1>SEATTLE SEAHAWKS</h1>
+                </div>
+            </section>
+        </button>
+        <button class="card">
+            <section id="img1">
+                <img id="packers-logo" src="" alt="" width="200" height="100">>
+                <div class="container">
+                    <h1>GREEN BAY PACKERS</h1>
+                </div>
+            </section>
+        </button>
+        <button class="card">
+            <section id="img1">
+                <img id="buccs-logo" src="" alt="" width="200" height="100">>
+                <div class="container">
+                    <h1>TAMPA BAY BUCCANEERS</h1>
+                </div>
+            </section>
+        </button>
+        <button class="card">
+            <section id="img1">
+                <img id="49ers-logo" src="" alt="" width="200" height="100">>
+                <div class="container">
+                    <h1>SAN FRANSISCO 49ERS</h1>
+                </div>
+            </section>
+        </button>
+        <button class="card">
+            <section id="img1">
+                <img id="chargers-logo" src="" alt="" width="200" height="100">>
+                <div class="container">
+                    <h1>LOS ANGELES CHARGERS</h1>
+                </div>
+            </section>
+        </button>
+        <button class="card">
+            <section id="img1">
+                <img id="patriots-logo" src="" alt="" width="200" height="100">>
+                <div class="container">
+                    <h1>NEW ENGLAND PATRIOTS</h1>
+                </div>
+            </section>
+        </button>
+        <button class="card">
+            <section id="img1">
+                <img id="dolphins-logo" src="" alt="" width="200" height="100">>
+                <div class="container">
+                    <h1>MIAMI DOLPHINS</h1>
+                </div>
+            </section>
+        </button>
+    );
+}
+
+export default Cards;
---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Nav() {
+    const linkStyle = { border: '1px black', padding: '5px' };
+
+    return (
+        <header>
+            <div class="topnav">
+            <h1 className="name-border">24/7 Football</h1>
+                <div class="search-container">
+                <form action="/action_page.php">
+                    <input type="text" placeholder="Search.." name="search">
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+        </header>
+    );
+}
+
