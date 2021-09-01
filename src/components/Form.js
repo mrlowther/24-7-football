178655e7cb536ab6a1d12063f73f4ba3e17fceb3
rangamboa <rangamboa@gmail.com>
2021-09-01 13:15:55 -0700

    finished initial contact page styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,130 +1,70 @@
-import React, { useState } from 'react';
-import { makeStyles, TextField, Typography} from '@material-ui/core';
+import React from 'react';
+import { Grid, Card, CardContent, makeStyles, TextField, Typography, Button} from '@material-ui/core';
 
-// Here we import a helper function that will check if the email is valid
-import { checkPassword, validateEmail } from '../utils/helpers';
-import { blockStatement, classExpression } from '@babel/types';
-
+const useStyles = makeStyles({
+  field: {
+      marginRight: 20,
+  }
+});
 
 function Form() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
-  const [email, setEmail] = useState('');
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const useStyles = makeStyles({
-    field: {
-        marginLeft: 20,
-        marginTop: 20,
-        marginBottom: 20,
-        display: 'block'
-    }
-  })
 
     const classes = useStyles();
 
-    const handleInputChange = (e) => {
-
-        const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
-
-        if (inputType === 'email') {
-        setEmail(inputValue);
-        } else if (inputType === 'userName') {
-        setUserName(inputValue);
-        } else {
-        setPassword(inputValue);
-        }
-    };
-
-    const handleFormSubmit = (e) => {
-
-        e.preventDefault();
-
-        if (!validateEmail(email) || !userName) {
-        setErrorMessage('Email or username is invalid.');
-        return;
-        }
-
-        if (!checkPassword(password)) {
-        setErrorMessage(`Choose a more secure password for the account: ${userName}.`);
-        return;
-        }
-
-        setUserName('');
-        setPassword('');
-        setEmail('');
-    };
-
     return (
     <>
-    <div>
-        <form noValidate autoComplete="off">
+        <Card style={{ maxWidth:500, margin:"0 auto" }}>
+            <CardContent>
+                <form>
+                <Grid container spacing={1}>
 
-            <TextField
-                className = {classes.field}
-                label="User Name"
-                variant="outlined"
-                required
-            />
+                    <Grid xs={12} sm={6} item>
+                        <TextField
+                            className = {classes.field}
+                            label="User Name"
+                            placeholder="Enter your username."
+                            variant="outlined"
+                            required
+                            fullWidth
+                        />
+                    </Grid>
 
-            <TextField
-                className = {classes.field}
-                label="Email"
-                variant="outlined"
-                required
-            />
+                    <Grid xs={12} sm={6} item>
+                        <TextField
+                            className = {classes.field}
+                            label="Email"
+                            type="email"
+                            placeholder="Enter your email."
+                            variant="outlined"
+                            required
+                            fullWidth
+                        />
+                    </Grid>
 
-            <TextField
-                className = {classes.field}
-                label="Message"
-                variant="outlined"
-                required
-                multiline
-                rows={3}
-                fullWidth
-            />
+                    <Grid xs={12} sm={12} item>
+                        <TextField
+                            className = {classes.field}
+                            label="Message"
+                            placeholder="What's on your mind?"
+                            variant="outlined"
+                            required
+                            multiline
+                            rows={4}
+                            fullWidth
+                        />
+                    </Grid>
 
-        </form>
-        <Typography variant="body2">*required field</Typography>
-    </div>
+                    <Grid xs={12} sm={12} item>
+                        <Button type="submit" variant="contained" color="primary">Submit</Button>
+                    </Grid>
 
-    {/*
-    <div>
-      <form className="form">
-        <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="email"
-          placeholder="email"
-        />
-        <input
-          value={userName}
-          name="userName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="username"
-        />
-        <input
-          value={password}
-          name="password"
-          onChange={handleInputChange}
-          type="password"
-          placeholder="Password"
-        />
-        <button type="button" onClick={handleFormSubmit}>Submit</button>
-      </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
-    </div>
-    */}
+                </Grid>
+
+                <Typography variant="body2">*All fields are required.</Typography>
+                </form>
+
+            </CardContent>
+        </Card>
     </>
   );
 }
