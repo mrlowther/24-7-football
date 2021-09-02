769e9e2221f9135d0bd01bd782761e1a71d9890d
mrlowther <85920943+mrlowther@users.noreply.github.com>
2021-09-02 16:17:52 -0700

    added routes to search by team, name, postition, and number

--- controllers/playerRoutes.js
+++ controllers/playerRoutes.js
@@ -22,14 +22,61 @@ router.get('/players', (req, res)=>{
     
 })
 
-
-router.get("/player", (req, res)=>{
-    Player.findOne({
+router.get("/players/team/:team", (req, res)=>{
+    Player.findAll({
         where:{
-            id: req.user.id
+            team: req.params.team
         }
-    }).then(userData=>{
-        res.json(userData);
+    }).then(playerData=>{
+        return res.json(playerData);
+    }).catch(err=>{
+        console.log(err);
+        return res.status(500).json({
+            message: "Error!", 
+            error: err
+        });
+    });
+})
+
+router.get("/players/position/:position", (req, res)=>{
+    Player.findAll({
+        where:{
+            position: req.params.position
+        }
+    }).then(playerData=>{
+        return res.json(playerData);
+    }).catch(err=>{
+        console.log(err);
+        return res.status(500).json({
+            message: "Error!", 
+            error: err
+        });
+    });
+})
+
+router.get("/players/name/:playerName", (req, res)=>{
+    Player.findAll({
+        where:{
+            playerName: req.params.playerName
+        }
+    }).then(playerData=>{
+        res.json(playerData);
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            message: "Error!", 
+            error: err
+        });
+    });
+})
+
+router.get("/players/number/:number", (req, res)=>{
+    Player.findAll({
+        where:{
+            number: req.params.number
+        }
+    }).then(playerData=>{
+        res.json(playerData);
     }).catch(err=>{
         console.log(err);
         res.status(500).json({
