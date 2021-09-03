5b23d7e54cc5409211f603f947a6ad97cef9309f
Lewis Capers <lewiscapersjr206@gmail.com>
2021-09-02 18:49:00 -0700

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Form from './Form';
-import Typography from '@material-ui/core/Typography';
-
-export default function Contact() {
-    return (
-      <>
-
-      <Typography variant="h3">Contact Us</Typography>
-      
-      <Form />
-
-      </>
-    );
-}
+//import React from 'react';
+//import Form from './Form';
+//import Typography from '@material-ui/core/Typography';
+//
+//export default function Contact() {
+//    return (
+//      <>
+//
+//      <Typography variant="h3">Contact Us</Typography>
+//      
+//      <Form />
+//
+//      </>
+//    );
+//}
