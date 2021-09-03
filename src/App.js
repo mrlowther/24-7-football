c4e366ee34e24e12772550848440ea780e5228fd
Lewis Capers <lewiscapersjr206@gmail.com>
2021-09-02 20:13:46 -0700

    update

--- src/App.js
+++ src/App.js
@@ -5,3 +5,35 @@ import MainContainer from "./components/MainContainer";
 const App = () => <MainContainer />;
 
 export default App;
+
+// import Nav from './components/Nav';
+// import FlipCard from './components/FlipCard';
+// 
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./components/styles.scss";
+// 
+// const cards = [
+//   {
+//     id: "2",
+//     variant: "click",
+//     front: "Click",
+//   },
+// ];
+// 
+// function App() {
+//   return (
+//     <div className="container">
+//       <Nav />
+// 
+//       <div className="container">
+//       <div className="row h-100">
+//         <div class="col d-flex flex-column flex-md-row justify-content-around align-items-center">
+//           {cards.map((card) => (
+//             <FlipCard key={card.id} card={card} />
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//     </div>
+//   );
+// }
