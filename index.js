5f108a7cde81699f05a77f7510e2d5624ede2e62
Tino Ganacias <84544540+tinoganacias@users.noreply.github.com>
2021-09-08 10:36:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const {User} = require("./models");
 const {Player} = require("./models");
 const allRoutes = require('./controllers');
